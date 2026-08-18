358f62d78e7d17a627df67ec2e39595c6f238050
MARIA EDUARDA MOREIRA TIAGO <maria.tiago@aluno.senac.rio>
2026-08-17 21:14:23 -0300

    teste1

---
+++ AULA 4/github.py
@@ -0,0 +1,9 @@
+#FAZER BACKUP SEMPRE!!
+#README.md = arquivo que adicionamos o github para explicar, tipo uma bula
+#extensão md= markdown 
+#para fazer a transferencia do vs para o git hub 
+#terminal = abrir a pasta = escolher a pasta = comando git init = 
+#após gerar o link de diretório na extensão git, fazer comando = git clone + link de diretorio gerado
+
+
+#local = pasta raiz / github = main branch
