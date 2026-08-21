ef176d26e5a57811605b14a8de27e5c97437894e
DudaMoreira021 <mariaeduardamoreira00@gmail.com>
2026-08-21 18:36:59 -0300

    Add files via upload

---
+++ AULA 5/part1.py
@@ -0,0 +1,13 @@
+#for = se comporta como uma estrutura que varre a variável
+#o for imprime na vertical, letra por letra - em strings = é uma estrutura de repetição, pega uma variável e coloca ela p trablhar em looping
+# palavra chave do dia = índice ou index
+#iterar = esse looping
+#para fazer a impressão de número o comando é = for i in range(x) --->> print (i)
+
+
+
+meunome = "Duda"
+
+for i in meunome:
+    print(i)
+
