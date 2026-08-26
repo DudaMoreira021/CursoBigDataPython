568dda8d35ecf3b61ea0faabb7199a48094a59b0
DudaMoreira021 <mariaeduardamoreira00@gmail.com>
2026-08-26 16:13:42 -0300

    Enhance comments and examples for for loop usage
    Added comments explaining the for loop and its usage. Included examples of for loops with range.

--- AULA 5/part1.py
+++ AULA 5/part1.py
@@ -11,3 +11,11 @@ meunome = "Duda"
 for i in meunome:
     print(i)
 
+# for i in range(10):
+#      print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+for i in range (0,10,1):
+       print(i)
