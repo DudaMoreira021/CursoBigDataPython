4af8debce24c924f07936af389fd482b677b5824
DudaMoreira021 <mariaeduardamoreira00@gmail.com>
2026-08-26 21:38:28 -0300

    Add files via upload

---
+++ AULA 8/part2.py
@@ -0,0 +1,27 @@
+# 26/08
+
+# Palavra chave = reutilizar
+
+# list / set / tuple / dict 
+
+#V.ENV
+# def calculadora_v1(): #aqui criamos uma função, tipo print, algo que comanda a operação
+#     calculadora_v1 () #fazemos isso para chamar a função, como fazemos com print.
+
+#     as variáveis usadas dentro do parentese da função é chamado de parametros ou argumentos
+
+
+# def calculadora_v1(num1,num2,operador) - deixamos a fn=unção assim quando não queremos usar variável
+# calculadora_v1 (200,500,"1") - colocamos os dados da variável de forma respectiva e respeitando a sinalização
+
+#def calculadora_v1(num1,num2,operador): se eu fizer def calculadora_v1(num1,num2,operador="1"): significa que em qualquer opção ele irá selecionar a opção que eu defini 
+#e será constante, não precisarei usar a opção no final = calculadora_v1 (200,500,"1")
+
+
+#possível erro NONE no terminal, parenteses sem nada
+
+
+
+# quando personalizamos uma variável, não aparece o erro NONE
+
+#return?
